f2b093537203940f66145114eb401a9ba22c2f30
Karl Horning <k.horning@me.com>
2025-06-22 16:32:58 +0100

    Push more test files!

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,28 +1,32 @@
-import fs from "fs";
-import path from "path";
-import postcss from "postcss";
-import tailwindcss from "@tailwindcss/postcss";
-
 export default function (eleventyConfig) {
-    eleventyConfig.on("eleventy.before", async () => {
-        const tailwindInputPath = path.resolve("./src/styles/index.css");
-        const tailwindOutputPath = "./dist/styles/index.css";
-        const cssContent = fs.readFileSync(tailwindInputPath, "utf8");
-        const outputDir = path.dirname(tailwindOutputPath);
-
-        if (!fs.existsSync(outputDir)) {
-            fs.mkdirSync(outputDir, { recursive: true });
-        }
-
-        const result = await postcss([tailwindcss()]).process(cssContent, {
-            from: tailwindInputPath,
-            to: tailwindOutputPath,
-        });
-
-        fs.writeFileSync(tailwindOutputPath, result.css);
+    eleventyConfig.addPassthroughCopy("src", {
+        //debug: true,
+        filter: [
+            "404.html",
+            "**/*.css",
+            "!**/*tailwind-input.css",
+            "**/*.js",
+            "**/*.json",
+            "!**/*.11ty.js",
+            "!**/*.11tydata.js",
+        ],
     });
 
+    // Copy img folder
+    eleventyConfig.addPassthroughCopy("src/img");
+
+    eleventyConfig.setServerPassthroughCopyBehavior("copy");
+
+    // tell 11ty which files to process and which files to copy while maintaining directory structure
+    // eleventyConfig.setTemplateFormats(["md","html","njk"]);
+
     return {
-        dir: { input: "src", output: "dist" },
+        dir: {
+            input: "src",
+            output: "dist",
+            // ⚠️ These values are both relative to your input directory.
+            includes: "_includes",
+            layouts: "_layouts",
+        },
     };
 }
